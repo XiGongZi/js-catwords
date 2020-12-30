ccf47571ecdd7f83f63956d07d32803b43cbc947
wangAnCheng <1079688386@qq.com>
2020-12-30 22:25:24 +0800

    test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fun = require("./src/libs/index.js");
-const words = require("./index.json");
-
-// 通过原始数据生成需要的字典
-fun.initWord(words);
-// 找词
-fun.init("纳斯腮红");
+// const words = require("./index.json");
+// // 通过原始数据生成需要的字典
+// fun.initWord(words);
+// // 找词
+// fun.init("纳斯腮红");
+modules.export = fun;
