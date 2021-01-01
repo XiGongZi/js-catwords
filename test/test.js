7202123e7a2ea4cd57677388d5864fb633d1ce7a
gongzixi <1079688386@qq.com>
2021-01-01 13:38:49 +0800

    add test

--- test/test.js
+++ test/test.js
@@ -4,54 +4,62 @@ var words = require("../index.json");
 
 catwords.initWord(words);
 
-it(`should return ["纳斯"]`, function()
+it(`纳斯 should return ["纳斯"]`, function()
 {
     var sum = catwords.init("纳斯");
     sum = JSON.stringify(sum);
-    console.log(sum);
+    console.log("result", sum);
     assert.equal(sum, `["纳斯"]`);
 });
 
-it(`should return ["纳斯","腮红"]`, function()
+it(`纳斯腮红 should return ["纳斯","腮红"]`, function()
 {
     var sum = catwords.init("纳斯腮红");
     sum = JSON.stringify(sum);
-    console.log(sum);
+    console.log("result", sum);
     assert.equal(sum, `["纳斯","腮红"]`);
 });
 
 
-it(`should return ["纳斯","腮红"]`, function()
+it(`纳斯1腮红 should return ["纳斯","腮红"]`, function()
 {
     var sum = catwords.init("纳斯1腮红");
     sum = JSON.stringify(sum);
-    console.log(sum);
+    console.log("result", sum);
     assert.equal(sum, `["纳斯","腮红"]`);
 });
 
 
-it(`should return ["纳斯","腮红"]`, function()
+it(`1纳斯3腮红4 should return ["纳斯","腮红"]`, function()
 {
     var sum = catwords.init("1纳斯3腮红4")
     sum = JSON.stringify(sum);
-    console.log(sum);
+    console.log("result", sum);
     assert.equal(sum, `["纳斯","腮红"]`);
 });
 
 
-it(`should return ["腮红"]`, function()
+it(`纳2斯腮红 should return ["腮红"]`, function()
 {
     var sum = catwords.init("纳2斯腮红");
     sum = JSON.stringify(sum);
-    console.log(sum);
+    console.log("result", sum);
     assert.equal(sum, `["腮红"]`);
 });
 
 
-it(`should return ["纳斯"]`, function()
+it(`纳斯腮2红 should return ["纳斯"]`, function()
 {
     var sum = catwords.init("纳斯腮2红");
     sum = JSON.stringify(sum);
-    console.log(sum);
+    console.log("result", sum);
+    assert.equal(sum, `["纳斯"]`);
+});
+
+it(`纳纳斯腮2红 should return ["纳斯"]`, function()
+{
+    var sum = catwords.init("纳纳斯腮2红");
+    sum = JSON.stringify(sum);
+    console.log("result", sum);
     assert.equal(sum, `["纳斯"]`);
 });
