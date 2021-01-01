c5aeba8fa47ec27bd0cdcc0644380da4367f9653
gongzixi <1079688386@qq.com>
2021-01-01 19:20:28 +0800

    remove console

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 const fun = require("./libs/index.js");
 "use strict";
-// const words = require("./index.json");
-// // 通过原始数据生成需要的字典
-// fun.initWord(words);
-// // 找词
-// fun.init("纳斯腮红");
+const words = require("../index.json");
+
+
+// console.log = function() {};
+
+// 通过原始数据生成需要的字典
+fun.initWord(words);
+// 找词
+fun.init("纳纳斯");
+// console.log(1)
 
 
 exports.initWord = fun.initWord;
